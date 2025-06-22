246cc3518142a41928bc8c524db7590c2ab8cf3a
NirnitaTalluri <nirnita12345@gmail.com>
2025-06-22 09:18:07 +0530

    Add files via upload
    Testleaf week1day1 assignments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		
+		System.out.println("chrome browser is developed by "+developer
+				+". The version is "+version
+				+" and the release year is "+releaseYear+" which is beta version "+isBeta+". Its shortcut key is "+shortcutKey);
+	}
+
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		int releaseYear = 1998;
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		char browserLogo = 'f';
+		
+		System.out.println("releaseYear: " +releaseYear);
+		System.out.println("browserVersion: " +browserVersion);
+		System.out.println("browserName: " +browserName);
+		System.out.println("isVisible: " +isVisible);
+		System.out.println("browserLogo: " +browserLogo);
+	}
+
+}
